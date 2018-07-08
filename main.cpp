d3b2de93c940d7a46d7849fcb3e76bf9f839dfd0
nahumroyer <nahumroyer@Nahums-MacBook-Pro.local>
2018-07-08 14:24:31 -0700

    objects and classes implemented

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class NahumsClass{
+public:
+    void coolSaying(){
+        cout << "The answer to eveything is 42" << endl;
+    }
+};
 int main() {
+    NahumsClass nahums_object;
     cout << "Hello, World!" << endl;
+    nahums_object.coolSaying();
     return 0;
 }
