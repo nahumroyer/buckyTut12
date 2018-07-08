92cf2c9cef729b57e96d8e0844b682613b906f5d
nahumroyer <nahumroyer@Nahums-MacBook-Pro.local>
2018-07-08 15:21:27 -0700

    Adding variables in class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class NahumsClass{
-public:
+public: 
+
     void coolSaying(){
-        cout << "The answer to eveything is 42" << endl;
+        cout << "The answer to everything is 42" << endl;
     }
+
+    // Since we don't have direct access to the variable name, we need
+    // a function to set name.
+    void setName(string x){
+        name = x;
+    }
+
+    // Since we don't have direct access to the variable name, we need
+    // a function to "get" / access name.
+    string getName(){
+        return name;
+    }
+
+private:
+    string name;
 };
 int main() {
+    // from tut12
     NahumsClass nahums_object;
     cout << "Hello, World!" << endl;
     nahums_object.coolSaying();
+
+    // from tut 13
+    nahums_object.setName("Sire Bucky Wallace");
+    cout << nahums_object.getName() << endl;
+
     return 0;
 }
