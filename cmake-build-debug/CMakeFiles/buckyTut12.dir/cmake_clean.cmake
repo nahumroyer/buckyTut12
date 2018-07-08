0f93499023092281ca0db1b960185dcd1fce7cca
nahumroyer <nahumroyer@Nahums-MacBook-Pro.local>
2018-07-08 11:47:47 -0700

    Initial

---
+++ cmake-build-debug/CMakeFiles/buckyTut12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buckyTut12.dir/main.cpp.o"
+  "buckyTut12.pdb"
+  "buckyTut12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buckyTut12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
